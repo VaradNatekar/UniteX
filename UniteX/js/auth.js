df5d531bc5b7a129a52bb9bbefd24e296a5b6d30
VaradNatekar <varadnatekar5507@gmail.com>
2026-02-21 23:43:10 +0530

    Update auth.js

---
+++ UniteX/js/auth.js
@@ -0,0 +1,12 @@
+function login() {
+  document.getElementById("loginMessage").innerText =
+    "Firebase not connected yet.";
+}
+
+function signup() {
+  document.getElementById("signupMessage").innerText =
+    "Firebase not connected yet.";
+}
+function logout() {
+  alert("Logout not connected to Firebase yet.");
+}
